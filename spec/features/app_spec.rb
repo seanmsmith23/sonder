f480f1c335526acf8c305f426192ab5f4bc9455a
Sean Smith <seansmith1020@gmail.com>
2014-08-15 16:05:11 -0600

    basic structure for registration page

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
-feature "this" do
-  scenario 'b' do
+feature "Welcome" do
+  scenario 'User should see a registration form and a button for signin' do
+    visit '/'
 
+    expect(page).to have_content("Sonder")
+    expect(page).to have_content("A community for remembering the ones we've lost")
+    expect(page).to have_content("First Name")
+    expect(page).to have_content("Last Name")
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password")
+    expect(page).to have_content("Confirm Password")
+    expect(page).to have_button("Sign Up")
+    expect(page).to have_content("Already signed up?")
+    expect(page).to have_link("Sign In")
   end
 end
