c6b946fdd26add5f0baeb267b391f236e5534992
Sean Smith <seansmith1020@gmail.com>
2014-08-17 13:16:54 -0600

    email validations display above form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,17 @@ class SessionsController < ApplicationController
 
   def create
 
+    @user = User.create(
+                        first_name: params[:user][:first_name],
+                        last_name: params[:user][:last_name],
+                        email: params[:user][:email],
+                        password: params[:user][:password],
+                        password_confirmation: params[:user][:password_confirm] )
+    if @user.valid?
+      @user.save
+      redirect_to 'new.js.erb'
+    else
+      render 'new'
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :first_name, :last_name, :email, :password_digest, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
 end
