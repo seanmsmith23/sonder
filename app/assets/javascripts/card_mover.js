3230a38b4b1c1e6be46fe95619e84ec3a7e74272
Sean Smith <seansmith1020@gmail.com>
2014-09-06 17:44:26 -0600

    cards are moved into one, two or three columns depending on the screen width

---
+++ app/assets/javascripts/card_mover.js
@@ -0,0 +1,42 @@
+function MoveCards(cards){
+
+  this.cards = cards
+
+  this.threeColumns = function(index, element) {
+    if (index % 3 == 0) {
+      $('.three-column-one').append(element);
+    }
+    else if (index % 3 == 1) {
+      $('.three-column-two').append(element);
+    }
+    else if (index % 3 == 2) {
+      $('.three-column-three').append(element);
+    }
+  };
+
+  this.twoColumns = function(index, element) {
+    if (index % 2 == 0) {
+      $('.two-column-one').append(element);
+    }
+    else if (index % 2 == 1) {
+      $('.two-column-two').append(element);
+    }
+  };
+
+  this.oneColumn = function(index, element) {
+    $('.one-column-one').append(element)
+  };
+
+  this.one = function(){
+    this.cards.each(this.oneColumn);
+  };
+
+  this.two = function(){
+    this.cards.each(this.twoColumns);
+  };
+
+  this.three = function(){
+    this.cards.each(this.threeColumns);
+  };
+
+}
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.all.destroy_all
+Memorial.all.destroy_all
+Membership.all.destroy_all
+Story.all.destroy_all
+Storylike.all.destroy_all
+
+User.create!(first_name: "Some", last_name: "User", email: "some@user.com", password: "password")
+User.create!(first_name: "Second", last_name: "User", email: "second@user.com", password: "password")
+User.create!(first_name: "Third", last_name: "User", email: "third@user.com", password: "password")
+
+Memorial.create!(user_id: 1, name: "Abraham Lincoln", born: (Date.today - 5000), died: (Date.today - 5))
+
+Membership.create!(user_id: 1, memorial_id: 1)
+Membership.create!(user_id: 2, memorial_id: 1)
+Membership.create!(user_id: 3, memorial_id: 1)
+
+Story.create!(user_id: 1, memorial_id: 1, story: "Gosh Abe was such a nice guy. He used to help homeless kittens get home safely.")
+Story.create!(user_id: 2, memorial_id: 1, story: "He was super duper honest that is for sure!")
+Story.create!(user_id: 3, memorial_id: 1, story: "I remember once when he walked five miles to bring me back a penny!")
+
+Storylike.create!(story_id: 1, user_id: 1)
+Storylike.create!(story_id: 1, user_id: 2)
+Storylike.create!(story_id: 1, user_id: 3)
+
+Storylike.create!(story_id: 2, user_id: 1)
+Storylike.create!(story_id: 2, user_id: 2)
+
+Storylike.create!(story_id: 3, user_id: 3)
+
+
