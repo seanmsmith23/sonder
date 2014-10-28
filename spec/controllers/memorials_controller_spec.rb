ef112f70b85b7189a130447dc668df25b950d08f
Sean Smith <seansmith1020@gmail.com>
2014-10-28 14:42:13 -0600

    spec for user being made admin automatically

---
+++ spec/controllers/memorials_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe MemorialsController do
+  let!(:user){ create_user }
+
+  before :each do
+    session[:user_id] = user.id
+  end
+
+  describe "POST create" do
+    it "should assign the creating user as administrator" do
+      expect(Administrator.all.count).to eq(0)
+
+      post :create, {memorial: {name: "Abe Lincoln", born: "10/01/2014", died: "10/05/2014"}}
+
+      expect(Administrator.all.count).to eq(1)
+      expect(Administrator.first.id).to eq(user.id)
+    end
+  end
+
+
+end
